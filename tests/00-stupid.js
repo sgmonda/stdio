bb6f10a5b88e3d5bbcbdd8cbdc894328fae83361
sgmonda <sgmonda@gmail.com>
2014-08-15 03:06:55 +0200

    Use an older version of jasmine-node

--- tests/00-stupid.js
+++ tests/00-stupid.js
@@ -2,9 +2,6 @@
 
 'use strict';
 
-require('jasmine-reporters');
-jasmine.getEnv().addReporter(new jasmine.ConsoleReporter(console.log));
-
 describe('Stupid tests', function () {
 
 	it('Basic synchronous true', function () {
