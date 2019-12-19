3e4569dac8e0fe6bb2d3df8ffc635b884991dba2
Sergio Garcia Mondaray <sgmonda@gmail.com>
2019-12-19 12:00:31 +0100

    Small refactor to improve code quality

--- src/ask.ts
+++ src/ask.ts
@@ -6,41 +6,56 @@ export interface AskConfig {
   inputStream?: any;
 }
 
-function print(text: string, options?: string[]): void {
+function getInputStream(config: AskConfig): any {
+  return config.inputStream || process.stdin;
+}
+
+function print(text: string, config: AskConfig): void {
+  const inputStream = getInputStream(config);
+  if (inputStream !== process.stdin) return;
+  const { options } = config;
   if (options) text += ' [' + options.join('/') + ']';
   text += ': ';
   process.stdout.write(text);
 }
 
-function ask(question: string, config: AskConfig = {}): Promise<string> {
-  const inputStream = config.inputStream || process.stdin;
-  return new Promise((resolve, reject) => {
-    const callback = (error: string, value?: string): void => (error ? reject(new Error(error)) : resolve(value));
-
-    let tries = config.maxRetries || DEFAULT_MAX_RETRIES;
-    inputStream.resume();
-
-    function listener(data: string): void {
-      const response = data.toString().trim();
-
-      if (config.options && !config.options.includes(response)) {
-        if (inputStream === process.stdin) console.log('Unexpected answer. %d retries left.', tries - 1);
-        tries--;
-        if (tries === 0) {
-          inputStream.removeListener('data', listener);
-          inputStream.pause();
-          callback('Retries spent');
-        } else {
-          print(question, config.options);
-        }
-        return;
-      }
+function getOnError(question: string, config: AskConfig, callback: Function): Function {
+  const inputStream = getInputStream(config);
+  return (listener: any, tries: number): void => {
+    if (inputStream === process.stdin) console.log('Unexpected answer. %d retries left.', tries - 1);
+    if (tries === 0) {
       inputStream.removeListener('data', listener);
       inputStream.pause();
-      callback('', response);
+      callback('Retries spent');
+    } else {
+      print(question, config);
     }
+  };
+}
+
+function getListener(question: string, config: AskConfig, callback: Function): (data: string) => void {
+  const inputStream = getInputStream(config);
+  const tries = config.maxRetries || DEFAULT_MAX_RETRIES;
+  const onError = getOnError(question, config, callback);
+
+  function listener(data: string): void {
+    const answer = data.toString().trim();
+    if (config.options && !config.options.includes(answer)) return onError(listener, tries - 1);
+    inputStream.removeListener('data', listener);
+    inputStream.pause();
+    callback('', answer);
+  }
+  return listener;
+}
+
+function ask(question: string, config: AskConfig = {}): Promise<string> {
+  return new Promise((resolve, reject) => {
+    const callback = (error: string, value?: string): void => (error ? reject(new Error(error)) : resolve(value));
+    const inputStream = getInputStream(config);
+    inputStream.resume();
+    const listener = getListener(question, config, callback);
     inputStream.addListener('data', listener);
-    print(question, config.options);
+    print(question, config);
   });
 }
 
